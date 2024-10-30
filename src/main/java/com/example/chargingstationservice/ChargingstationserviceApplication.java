5ec7820f187495846c29ff3a1f89594ecd0dcf62
valtechadmin <valtechadmin@valtechadmins-MacBook-Pro.local>
2024-10-30 15:20:40 +0530

    Added project folder

---
+++ src/main/java/com/example/chargingstationservice/ChargingstationserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.chargingstationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargingstationserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargingstationserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chargingstationservice/ChargingstationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chargingstationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingstationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
